6b85366aa3d41c60ec2ffd24f5ed431183917437
hyeonbin <basilla0617@naver.com>
2024-03-13 21:00:00 +0900

    feat: 절차 지향 프로그래밍2 - 데이터 묶음

---
+++ src/oop1/MusicPlayerData.java
@@ -0,0 +1,8 @@
+package oop1;
+
+/** MusicPlayerMain1 코드에 클래스 도입.
+    MusicPlayerData 클래스를 만들고, 음악 플레이어에 사용되는 데이터들을 묶어 멤버 변수로 사용하자. **/
+public class MusicPlayerData {
+    int volume = 0;
+    boolean isOn = false;
+}
